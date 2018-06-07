5f1761082fcfd1ad3962a87741d47d8225c4d7ea
Syed Faraaz Ahmad <faraaz98@live.com>
2018-06-07 17:34:41 +0530

    add code for prediction

--- ex2/predict.m
+++ ex2/predict.m
@@ -15,10 +15,15 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+s = sigmoid(X * theta);
 
-
-
-
+for i = 1:m
+  if s(i) >= 0.5
+    p(i) = 1;
+  else
+    p(i) = 0;
+  endif
+endfor
 
 
 % =========================================================================
