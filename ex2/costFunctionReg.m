1cb10c74b89faeb820db09f33733f8390f8887a5
Syed Faraaz Ahmad <faraaz98@live.com>
2018-06-07 21:00:10 +0530

    complete ex2

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,10 +17,28 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X*theta);
+n = size(theta);
 
+for i = 1:m
+  J += -y(i) * log(h(i)) - (1 - y(i)) * log(1 - h(i));
+endfor
+J /= m;
 
+for j = 2:n
+  J += (lambda / (2 * m)) * theta(j) ^ 2;
+endfor
 
+for i = 1:m
+  grad(1) += (1 / m) * (h(i) - y(i)) * X(i, 1); 
+endfor
 
+for j = 2:n
+  for i = 1:m
+    grad(j) += (h(i) - y(i)) * X(i, j) + (lambda / m) * theta(j);
+  endfor
+  grad(j) /= m;
+endfor
 
 % =============================================================
 
